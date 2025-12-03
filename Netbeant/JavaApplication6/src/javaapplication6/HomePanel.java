43980ae358927e62e60dcf3354f6727144462272
Arif <dopymonster@gamil.com>
2025-12-03 10:43:04 +0800

    .

--- Netbeant/JavaApplication6/src/javaapplication6/HomePanel.java
+++ Netbeant/JavaApplication6/src/javaapplication6/HomePanel.java
@@ -63,7 +63,7 @@ public class HomePanel extends javax.swing.JPanel {
                     .addComponent(jLabel3)
                     .addComponent(jLabel4)
                     .addComponent(jLabel5))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 57, Short.MAX_VALUE)
                 .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 306, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(32, 32, 32))
         );
