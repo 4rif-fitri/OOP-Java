98e5450411771627809723dda7915fdff9c39837
Arif <dopymonster@gamil.com>
2025-12-30 20:15:46 +0800

    asd

--- [8] inheritance/Lab/pet/PetDemo.java
+++ [8] inheritance/Lab/pet/PetDemo.java
@@ -1,14 +1,11 @@
 package Lab.Pet;
 
-import Lab.Pet.Cat;
-import Lab.Pet.Dog;
-
 public class PetDemo {
     public static void main(String[] args) {
         Dog goofy = new Dog();
         Dog scooby = new Dog("scooby Doo", "chocolate");
         Cat doremon = new Cat();
-
+//
         goofy.display();
         scooby.display();
         doremon.display();
